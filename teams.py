c1dabdbfcd2d6988a3a6850bf2923664df6d3fcb
Andrew Boyer <asboyer@gmail.com>
2022-08-01 17:09:24 -0400

    working

--- teams.py
+++ teams.py
@@ -26,4 +26,32 @@ teams = []
 for team in teams_div:
     teams.append(str(team))
 with open('test1.txt', 'w+') as file:
-    file.write(teams)
+    for team in teams:
+        file.write(team)
+        file.write("\n__________________\n")
+
+stop_index = 0
+header_str = teams.pop(0)
+for i in range(len(teams)):
+    if teams[i] == header_str:
+        stop_index = i
+        break
+teams = teams[0:i]
+
+new_teams = []
+for team in teams:
+    soup1 = BeautifulSoup(team, 'html.parser')
+    new_teams.append(soup1.findAll('a')[0]['href'].split("/")[2])
+
+with open('test1.txt', 'w+') as file:
+    for team in new_teams:
+        file.write(team)
+        file.write("\n__________________\n")
+
+s = {
+    "teams": new_teams
+}
+
+with open('teams.json', 'w+') as file:
+    file.write(json.dumps(s, indent=4, ensure_ascii=False))
+
